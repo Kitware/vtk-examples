26430c4a3c9bb70767265dc8ebd48367a026dde1
Andrew Maclean <andrew.amaclean@gmail.com>
2020-06-26 16:52:21 +1000

    SYNC: Files modified in the repo.
    Former-commit-id: f5750d48256d2fefbcdd474ad7a9cf03fe7a4970

--- src/SyncSiteWithRepo.sh
+++ src/SyncSiteWithRepo.sh
@@ -66,8 +66,8 @@ echo "6.1 Modify highlight color"
 (cd site/assets/stylesheets; sed -i -e 's/background-color:rgba(255,235,59,\.5)/background-color:rgba(240,240,240,.8)/g' application-*.css)
 
 #####################
- echo "Premature exit for testing"
- exit
+# echo "Premature exit for testing"
+# exit
 
 #####################
 echo "7) Minify Html"
