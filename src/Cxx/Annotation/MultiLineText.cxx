9bb63ca85f23bef21f9c3b6f5b85003eae590a31
Andrew Maclean <andrew.amaclean@gmail.com>
2020-09-15 15:16:38 +1000

    Modernising the code

--- src/Cxx/Annotation/MultiLineText.cxx
+++ src/Cxx/Annotation/MultiLineText.cxx
@@ -2,13 +2,12 @@
 // vtkTextMappers.  It shows several justifications as well as
 // single-line and multiple-line text inputs.
 
-#include <vtkSmartPointer.h>
-
-#include <vtkNamedColors.h>
 #include <vtkActor2D.h>
 #include <vtkCamera.h>
 #include <vtkCellArray.h>
 #include <vtkCoordinate.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
 #include <vtkPoints.h>
 #include <vtkPolyData.h>
 #include <vtkPolyDataMapper2D.h>
@@ -19,16 +18,15 @@
 #include <vtkTextMapper.h>
 #include <vtkTextProperty.h>
 
-int main (int, char*[])
+int main(int, char*[])
 {
-  int font_size = 24;;
+  int font_size = 24;
 
-// Create the text mappers and the associated Actor2Ds.
+  // Create the text mappers and the associated Actor2Ds.
 
-// The font and text properties (except justification) are the same for
-// each single line mapper. Let's create a common text property object
-  vtkSmartPointer<vtkTextProperty> singleLineTextProp =
-    vtkSmartPointer<vtkTextProperty>::New();
+  // The font and text properties (except justification) are the same for
+  // each single line mapper. Let's create a common text property object
+  vtkNew<vtkTextProperty> singleLineTextProp;
 
   singleLineTextProp->SetFontSize(font_size);
   singleLineTextProp->SetFontFamilyToArial();
@@ -36,10 +34,9 @@ int main (int, char*[])
   singleLineTextProp->ItalicOff();
   singleLineTextProp->ShadowOff();
 
-// The font and text properties (except justification) are the same for
-// each multi line mapper. Let's create a common text property object
-  vtkSmartPointer<vtkTextProperty> multiLineTextProp =
-    vtkSmartPointer<vtkTextProperty>::New();
+  // The font and text properties (except justification) are the same for
+  // each multi line mapper. Let's create a common text property object
+  vtkNew<vtkTextProperty> multiLineTextProp;
 
   multiLineTextProp->ShallowCopy(singleLineTextProp);
   multiLineTextProp->BoldOn();
@@ -47,58 +44,51 @@ int main (int, char*[])
   multiLineTextProp->ShadowOn();
   multiLineTextProp->SetLineSpacing(0.8);
 
-  vtkSmartPointer<vtkNamedColors> colors =
-    vtkSmartPointer<vtkNamedColors>::New();
+  vtkNew<vtkNamedColors> colors;
 
-// The text is on a single line and bottom-justified.
-  vtkSmartPointer<vtkTextMapper> singleLineTextB =
-    vtkSmartPointer<vtkTextMapper>::New();
+  // The text is on a single line and bottom-justified.
+  vtkNew<vtkTextMapper> singleLineTextB;
   singleLineTextB->SetInput("Single line (bottom)");
-  vtkTextProperty *tprop = singleLineTextB->GetTextProperty();
-  tprop->ShallowCopy(singleLineTextProp);;
+  auto tprop = singleLineTextB->GetTextProperty();
+  tprop->ShallowCopy(singleLineTextProp);
+ 
   tprop->SetVerticalJustificationToBottom();
   tprop->SetColor(colors->GetColor3d("Tomato").GetData());
 
-  vtkSmartPointer<vtkActor2D> singleLineTextActorB =
-    vtkSmartPointer<vtkActor2D>::New();
+  vtkNew<vtkActor2D> singleLineTextActorB;
   singleLineTextActorB->SetMapper(singleLineTextB);
   singleLineTextActorB->GetPositionCoordinate()->SetCoordinateSystemToNormalizedDisplay();
   singleLineTextActorB->GetPositionCoordinate()->SetValue(0.05, 0.85);
 
-// The text is on a single line and center-justified (vertical
-// justification).
-  vtkSmartPointer<vtkTextMapper> singleLineTextC =
-    vtkSmartPointer<vtkTextMapper>::New();
+  // The text is on a single line and center-justified (vertical
+  // justification).
+  vtkNew<vtkTextMapper> singleLineTextC;
   singleLineTextC->SetInput("Single line (centered)");
   tprop = singleLineTextC->GetTextProperty();
   tprop->ShallowCopy(singleLineTextProp);
   tprop->SetVerticalJustificationToCentered();
   tprop->SetColor(colors->GetColor3d("DarkGreen").GetData());
-  vtkSmartPointer<vtkActor2D> singleLineTextActorC =
-    vtkSmartPointer<vtkActor2D>::New();
+  vtkNew<vtkActor2D> singleLineTextActorC;
 
   singleLineTextActorC->SetMapper(singleLineTextC);
   singleLineTextActorC->GetPositionCoordinate()->SetCoordinateSystemToNormalizedDisplay();
   singleLineTextActorC->GetPositionCoordinate()->SetValue(0.05, 0.75);
 
-// The text is on a single line and top-justified.
-  vtkSmartPointer<vtkTextMapper> singleLineTextT =
-    vtkSmartPointer<vtkTextMapper>::New();
+  // The text is on a single line and top-justified.
+  vtkNew<vtkTextMapper> singleLineTextT;
   singleLineTextT->SetInput("Single line (top)");
   tprop = singleLineTextT->GetTextProperty();
   tprop->ShallowCopy(singleLineTextProp);
   tprop->SetVerticalJustificationToTop();
   tprop->SetColor(colors->GetColor3d("Peacock").GetData());
 
-  vtkSmartPointer<vtkActor2D> singleLineTextActorT =
-    vtkSmartPointer<vtkActor2D>::New();
+  vtkNew<vtkActor2D> singleLineTextActorT;
   singleLineTextActorT->SetMapper(singleLineTextT);
   singleLineTextActorT->GetPositionCoordinate()->SetCoordinateSystemToNormalizedDisplay();
   singleLineTextActorT->GetPositionCoordinate()->SetValue(0.05, 0.65);
 
-// The text is on multiple lines and left- and top-justified.
-  vtkSmartPointer<vtkTextMapper> textMapperL =
-    vtkSmartPointer<vtkTextMapper>::New();
+  // The text is on multiple lines and left- and top-justified.
+  vtkNew<vtkTextMapper> textMapperL;
   textMapperL->SetInput("This is\nmulti-line\ntext output\n(left-top)");
   tprop = textMapperL->GetTextProperty();
   tprop->ShallowCopy(multiLineTextProp);
@@ -106,16 +96,14 @@ int main (int, char*[])
   tprop->SetVerticalJustificationToTop();
   tprop->SetColor(colors->GetColor3d("Tomato").GetData());
 
-  vtkSmartPointer<vtkActor2D> textActorL =
-    vtkSmartPointer<vtkActor2D>::New();
+  vtkNew<vtkActor2D> textActorL;
   textActorL->SetMapper(textMapperL);
   textActorL->GetPositionCoordinate()->SetCoordinateSystemToNormalizedDisplay();
   textActorL->GetPositionCoordinate()->SetValue(0.05, 0.5);
 
-// The text is on multiple lines and center-justified (both horizontal and
-// vertical).
-  vtkSmartPointer<vtkTextMapper> textMapperC =
-    vtkSmartPointer<vtkTextMapper>::New();
+  // The text is on multiple lines and center-justified (both horizontal and
+  // vertical).
+  vtkNew<vtkTextMapper> textMapperC;
   textMapperC->SetInput("This is\nmulti-line\ntext output\n(centered)");
   tprop = textMapperC->GetTextProperty();
   tprop->ShallowCopy(multiLineTextProp);
@@ -123,15 +111,13 @@ int main (int, char*[])
   tprop->SetVerticalJustificationToCentered();
   tprop->SetColor(colors->GetColor3d("DarkGreen").GetData());
 
-  vtkSmartPointer<vtkActor2D> textActorC =
-    vtkSmartPointer<vtkActor2D>::New();
+  vtkNew<vtkActor2D> textActorC;
   textActorC->SetMapper(textMapperC);
   textActorC->GetPositionCoordinate()->SetCoordinateSystemToNormalizedDisplay();
   textActorC->GetPositionCoordinate()->SetValue(0.5, 0.5);
 
-// The text is on multiple lines and right- and bottom-justified.
-  vtkSmartPointer<vtkTextMapper> textMapperR =
-    vtkSmartPointer<vtkTextMapper>::New();
+  // The text is on multiple lines and right- and bottom-justified.
+  vtkNew<vtkTextMapper> textMapperR;
   textMapperR->SetInput("This is\nmulti-line\ntext output\n(right-bottom)");
   tprop = textMapperR->GetTextProperty();
   tprop->ShallowCopy(multiLineTextProp);
@@ -139,17 +125,15 @@ int main (int, char*[])
   tprop->SetVerticalJustificationToBottom();
   tprop->SetColor(colors->GetColor3d("Peacock").GetData());
 
-  vtkSmartPointer<vtkActor2D> textActorR =
-    vtkSmartPointer<vtkActor2D>::New();
+  vtkNew<vtkActor2D> textActorR;
   textActorR->SetMapper(textMapperR);
   textActorR->GetPositionCoordinate()->SetCoordinateSystemToNormalizedDisplay();
   textActorR->GetPositionCoordinate()->SetValue(0.95, 0.5);
 
-// Draw the grid to demonstrate the placement of the text.
+  // Draw the grid to demonstrate the placement of the text.
 
-// Set up the necessary points.
-  vtkSmartPointer<vtkPoints> Pts =
-    vtkSmartPointer<vtkPoints>::New();
+  // Set up the necessary points.
+  vtkNew<vtkPoints> Pts;
   Pts->InsertNextPoint(0.05, 0.0, 0.0);
   Pts->InsertNextPoint(0.05, 1.0, 0.0);
   Pts->InsertNextPoint(0.5, 0.0, 0.0);
@@ -165,9 +149,8 @@ int main (int, char*[])
   Pts->InsertNextPoint(0.00, 0.65, 0.0);
   Pts->InsertNextPoint(0.50, 0.65, 0.0);
 
-// Set up the lines that use these points.
-  vtkSmartPointer<vtkCellArray> Lines =
-    vtkSmartPointer<vtkCellArray>::New();
+  // Set up the lines that use these points.
+  vtkNew<vtkCellArray> Lines;
   Lines->InsertNextCell(2);
   Lines->InsertCellPoint(0);
   Lines->InsertCellPoint(1);
@@ -190,39 +173,32 @@ int main (int, char*[])
   Lines->InsertCellPoint(12);
   Lines->InsertCellPoint(13);
 
-// Create a grid that uses these points and lines.
-  vtkSmartPointer<vtkPolyData> Grid =
-    vtkSmartPointer<vtkPolyData>::New();
+  // Create a grid that uses these points and lines.
+  vtkNew<vtkPolyData> Grid;
   Grid->SetPoints(Pts);
   Grid->SetLines(Lines);
-// Set up the coordinate system.
-  vtkSmartPointer<vtkCoordinate> normCoords =
-    vtkSmartPointer<vtkCoordinate>::New();
+  // Set up the coordinate system.
+  vtkNew<vtkCoordinate> normCoords;
   normCoords->SetCoordinateSystemToNormalizedViewport();
 
-// Set up the mapper and actor (2D) for the grid.
-  vtkSmartPointer<vtkPolyDataMapper2D> mapper =
-    vtkSmartPointer<vtkPolyDataMapper2D>::New();
+  // Set up the mapper and actor (2D) for the grid.
+  vtkNew<vtkPolyDataMapper2D> mapper;
   mapper->SetInputData(Grid);
   mapper->SetTransformCoordinate(normCoords);
-  vtkSmartPointer<vtkActor2D> gridActor =
-    vtkSmartPointer<vtkActor2D>::New();
+  vtkNew<vtkActor2D> gridActor;
   gridActor->SetMapper(mapper);
   gridActor->GetProperty()->SetColor(colors->GetColor3d("DimGray").GetData());
 
-// Create the Renderer, RenderWindow, and RenderWindowInteractor
-  vtkSmartPointer<vtkRenderer> renderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
+  // Create the Renderer, RenderWindow, and RenderWindowInteractor
+  vtkNew<vtkRenderer> renderer;
+  vtkNew<vtkRenderWindow> renderWindow;
 
   renderWindow->AddRenderer(renderer);
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  vtkNew<vtkRenderWindowInteractor> interactor;
   interactor->SetRenderWindow(renderWindow);
 
-// Add the actors to the renderer; set the background and size; zoom in
-// closer to the image; render
+  // Add the actors to the renderer; set the background and size; zoom in
+  // closer to the image; render
   renderer->AddActor2D(textActorL);
   renderer->AddActor2D(textActorC);
   renderer->AddActor2D(textActorR);
@@ -236,6 +212,8 @@ int main (int, char*[])
   renderer->GetActiveCamera()->Zoom(1.5);
 
   interactor->Initialize();
+  renderWindow->SetWindowName("MultiLineText");
+
   renderWindow->Render();
   interactor->Start();
 
