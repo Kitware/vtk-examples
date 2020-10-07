4647bef7809d744e54e7ae2b4e97b75d646e7192
Andrew Maclean <andrew.amaclean@gmail.com>
2020-10-08 09:27:01 +1100

    Modernising the code, improving images

--- src/Cxx/Lighting/LightActor.cxx
+++ src/Cxx/Lighting/LightActor.cxx
@@ -1,29 +1,47 @@
+#include <vtkCamera.h>
+#include <vtkLight.h>
+#include <vtkLightActor.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+
 #include <cstdlib>
 
-#include <vtkRenderer.h>
-#include <vtkRenderWindow.h>
-#include <vtkLightActor.h>
-#include <vtkLight.h>
-#include <vtkSmartPointer.h>
-#include <vtkRenderWindowInteractor.h>
-
-int main(int, char *[])
+int main(int, char*[])
 {
-  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+  vtkNew<vtkNamedColors> colors;
+
+  vtkNew<vtkRenderer> renderer;
+  renderer->SetBackground(colors->GetColor3d("Black").GetData());
 
   // Display where the light is
-  vtkSmartPointer<vtkLight> light = vtkSmartPointer<vtkLight>::New();
+  vtkNew<vtkLight> light;
   light->SetPositional(true); // without this line, the program crashes
-  vtkSmartPointer<vtkLightActor> lightActor = vtkSmartPointer<vtkLightActor>::New();
+  vtkNew<vtkLightActor> lightActor;
   lightActor->SetLight(light);
   renderer->AddViewProp(lightActor);
+  lightActor->GetFrustumProperty()->SetColor(
+      colors->GetColor3d("LavenderBlush").GetData());
 
-  vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+  vtkNew<vtkRenderWindow> renderWindow;
   renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("LightActor");
 
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-      vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
   renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  renderer->ResetCamera();
+  renderer->ResetCameraClippingRange();
+
+  auto camera = renderer->GetActiveCamera();
+  camera->SetPosition(-2.17199, -2.50774, 2.18);
+  camera->SetFocalPoint(-0.144661, -0.146372, 0.180482);
+  camera->SetViewUp(0.0157883, 0.638203, 0.769706);
+  camera->SetDistance(3.69921);
+  camera->SetClippingRange(1.76133, 6.14753);
 
   renderWindow->Render();
   renderWindowInteractor->Start();
--- src/Cxx/Lighting/SpotLights.cxx
+++ src/Cxx/Lighting/SpotLights.cxx
@@ -1,65 +1,56 @@
-#include <vtkSmartPointer.h>
-
 #include <vtkActor.h>
 #include <vtkCamera.h>
 #include <vtkLight.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
 #include <vtkPlaneSource.h>
 #include <vtkPolyDataMapper.h>
-#include <vtkNamedColors.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
 
-int main (int, char *[])
+int main(int, char*[])
 {
   // create a rendering window and renderer
-  vtkSmartPointer<vtkRenderer> renderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
+  vtkNew<vtkRenderer> renderer;
+  vtkNew<vtkRenderWindow> renderWindow;
   renderWindow->SetSize(640, 480);
   renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("SpotLights");
 
   // create a renderwindowinteractor
-  vtkSmartPointer<vtkRenderWindowInteractor>   iren =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  vtkNew<vtkRenderWindowInteractor> iren;
   iren->SetRenderWindow(renderWindow);
 
   // create a plane and add it to the renderer
-  vtkSmartPointer<vtkPlaneSource>   source =
-    vtkSmartPointer<vtkPlaneSource>::New();
+  vtkNew<vtkPlaneSource> source;
   source->SetOrigin(-5.0, 0.0, 5.0);
   source->SetPoint1(5.0, 0.0, 5.0);
   source->SetPoint2(-5.0, 0.0, -5.0);
   source->SetResolution(100, 100);
-  vtkSmartPointer<vtkPolyDataMapper>   mapper =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
+  vtkNew<vtkPolyDataMapper> mapper;
   mapper->SetInputConnection(source->GetOutputPort());
 
-  vtkSmartPointer<vtkNamedColors> colors =
-    vtkSmartPointer<vtkNamedColors>::New();
+  vtkNew<vtkNamedColors> colors;
 
-  vtkSmartPointer<vtkActor>   actor =
-    vtkSmartPointer<vtkActor>::New();
+  vtkNew<vtkActor> actor;
   actor->SetMapper(mapper);
   renderer->AddActor(actor);
-  
+
   // set camera view
   renderer->ResetCamera();
   renderer->GetActiveCamera()->Azimuth(40.0);
   renderer->GetActiveCamera()->Elevation(30.0);
 
   // create a green light
-  vtkSmartPointer<vtkLight> greenLight =
-    vtkSmartPointer<vtkLight>::New();
+  vtkNew<vtkLight> greenLight;
   greenLight->PositionalOn();
   greenLight->SetPosition(-4.0, 4.0, -1.0);
   greenLight->SetColor(colors->GetColor3d("Lawn_green").GetData());
   greenLight->SetIntensity(0.5);
 
   // create a pink light
-  vtkSmartPointer<vtkLight> pinkLight =
-    vtkSmartPointer<vtkLight>::New();
+  vtkNew<vtkLight> pinkLight;
   pinkLight->PositionalOn();
   pinkLight->SetPosition(4.0, 5.0, 1.0);
   pinkLight->SetColor(colors->GetColor3d("DeepPink").GetData());
