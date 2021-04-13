1f1eb2771a221b159a632fbf144f250d49ebb5d2
Andrew Maclean <andrew.amaclean@gmail.com>
2021-04-14 09:45:46 +1000

    Updating to vtkDiscreteFlyingEdges3D

--- src/Python/DataManipulation/MeshLabelImageColor.py
+++ src/Python/DataManipulation/MeshLabelImageColor.py
@@ -2,17 +2,20 @@ import vtk
 
 
 def main():
+    # vtkFlyingEdges3D was introduced in VTK >= 8.2
+    use_flying_edges = vtk_version_ok(8, 2, 0)
+
     colors = vtk.vtkNamedColors()
     ifn, index = get_program_parameters()
 
     # Prepare to read the file.
-    readerVolume = vtk.vtkMetaImageReader()
-    readerVolume.SetFileName(ifn)
-    readerVolume.Update()
+    reader_volume = vtk.vtkMetaImageReader()
+    reader_volume.SetFileName(ifn)
+    reader_volume.Update()
 
     # Extract the region of interest.
     voi = vtk.vtkExtractVOI()
-    voi.SetInputConnection(readerVolume.GetOutputPort())
+    voi.SetInputConnection(reader_volume.GetOutputPort())
     voi.SetVOI(0, 517, 0, 228, 0, 392)
     voi.SetSampleRate(1, 1, 1)
     voi.Update()  # Necessary for GetScalarRange().
@@ -20,7 +23,14 @@ def main():
     print('Range', srange)
 
     # Prepare surface generation.
-    contour = vtk.vtkDiscreteMarchingCubes()  # For label images.
+    # For label images.
+    if use_flying_edges:
+        try:
+            contour = vtk.vtkDiscreteFlyingEdges3D()
+        except AttributeError:
+            contour = vtk.vtkDiscreteMarchingCubes()
+    else:
+        contour = vtk.vtkDiscreteMarchingCubes()
     contour.SetInputConnection(voi.GetOutputPort())
     # contour.ComputeNormalsOn()
 
@@ -60,31 +70,31 @@ def main():
     maxz = 3.85
 
     # Create the color map.
-    colorLookupTable = vtk.vtkLookupTable()
-    colorLookupTable.SetTableRange(minz, maxz)  # This does nothing, use mapper.SetScalarRange(minz, maxz).
-    colorLookupTable.SetHueRange(2 / 3.0, 1)
-    # colorLookupTable.SetSaturationRange(0, 0)
-    # colorLookupTable.SetValueRange(1, 0)
-    # colorLookupTable.SetNumberOfColors(256) #256 default
-    colorLookupTable.Build()
+    lut = vtk.vtkLookupTable()
+    lut.SetTableRange(minz, maxz)  # This does nothing, use mapper.SetScalarRange(minz, maxz).
+    lut.SetHueRange(2 / 3.0, 1)
+    # lut.SetSaturationRange(0, 0)
+    # lut.SetValueRange(1, 0)
+    # lut.SetNumberOfColors(256) #256 default
+    lut.Build()
 
     # Calculate cell normals.
-    triangleCellNormals = vtk.vtkPolyDataNormals()
-    triangleCellNormals.SetInputData(smoothed_polys)
-    triangleCellNormals.ComputeCellNormalsOn()
-    triangleCellNormals.ComputePointNormalsOff()
-    triangleCellNormals.ConsistencyOn()
-    triangleCellNormals.AutoOrientNormalsOn()
-    triangleCellNormals.Update()  # Creates vtkPolyData.
+    triangle_cell_normals = vtk.vtkPolyDataNormals()
+    triangle_cell_normals.SetInputData(smoothed_polys)
+    triangle_cell_normals.ComputeCellNormalsOn()
+    triangle_cell_normals.ComputePointNormalsOff()
+    triangle_cell_normals.ConsistencyOn()
+    triangle_cell_normals.AutoOrientNormalsOn()
+    triangle_cell_normals.Update()  # Creates vtkPolyData.
 
     mapper = vtk.vtkPolyDataMapper()
     # mapper.SetInput(smoothed_polys) # This has no normals.
-    mapper.SetInputConnection(triangleCellNormals.GetOutputPort())  # this is better for vis;-)
+    mapper.SetInputConnection(triangle_cell_normals.GetOutputPort())  # this is better for vis;-)
     mapper.ScalarVisibilityOn()  # Show colour.
     mapper.SetScalarRange(minz, maxz)
     # mapper.SetScalarModeToUseCellData() # Contains the label eg. 31
     mapper.SetScalarModeToUsePointData()  # The smoother error relates to the verts.
-    mapper.SetLookupTable(colorLookupTable)
+    mapper.SetLookupTable(lut)
 
     # Take the isosurface data and create geometry.
     actor = vtk.vtkLODActor()
@@ -97,13 +107,13 @@ def main():
     ren.AddActor(actor)
 
     # Create a window for the renderer of size 600X600
-    renWin = vtk.vtkRenderWindow()
-    renWin.AddRenderer(ren)
-    renWin.SetSize(600, 600)
+    ren_win = vtk.vtkRenderWindow()
+    ren_win.AddRenderer(ren)
+    ren_win.SetSize(600, 600)
 
     # Set a user interface interactor for the render window.
     iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(renWin)
+    iren.SetRenderWindow(ren_win)
 
     # Start the initialization and rendering.
     iren.Initialize()
@@ -111,9 +121,9 @@ def main():
     ren.GetActiveCamera().SetFocalPoint(0.000271, -0.001974, 0.006892)
     ren.GetActiveCamera().SetViewUp(0.790211, -0.243999, 0.562166)
     ren.ResetCameraClippingRange()
-    renWin.Render()
-    renWin.SetWindowName('MeshLabelImageColor')
-    renWin.Render()
+    ren_win.Render()
+    ren_win.SetWindowName('MeshLabelImageColor')
+    ren_win.Render()
 
     iren.Start()
 
@@ -130,6 +140,28 @@ def get_program_parameters():
     parser.add_argument('label', nargs='?', const=1, type=int, default=31, help='The label to use e.g 31')
     args = parser.parse_args()
     return args.filename, args.label
+
+
+def vtk_version_ok(major, minor, build):
+    """
+    Check the VTK version.
+
+    :param major: Major version.
+    :param minor: Minor version.
+    :param build: Build version.
+    :return: True if the requested VTK version is greater or equal to the actual VTK version.
+    """
+    needed_version = 10000000000 * int(major) + 100000000 * int(minor) + int(build)
+    try:
+        vtk_version_number = vtk.VTK_VERSION_NUMBER
+    except AttributeError:  # as error:
+        ver = vtk.vtkVersion()
+        vtk_version_number = 10000000000 * ver.GetVTKMajorVersion() + 100000000 * ver.GetVTKMinorVersion() \
+                             + ver.GetVTKBuildVersion()
+    if vtk_version_number >= needed_version:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
