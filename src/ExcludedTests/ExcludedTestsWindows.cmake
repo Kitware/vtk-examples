4e127f8c5989cb1d0a07338332ab3671d6bb37b0
Andrew Maclean <andrew.amaclean@gmail.com>
2021-05-28 09:44:20 +1000

    Add links to issues and discourse

--- src/ExcludedTests/ExcludedTestsWindows.cmake
+++ src/ExcludedTests/ExcludedTestsWindows.cmake
@@ -1,5 +1,7 @@
 # These are tests to be excluded from testing that are specific to Windows
-# Meshes-InterpolateFieldDataDemo: Test file has UTF-8 characters, fails in Windows
+# Meshes-InterpolateFieldDataDemo: Test file has UTF-8 characters,
+#  fails in Windows. See: https://gitlab.kitware.com/vtk/vtk/-/issues/18026
+#  and https://discourse.vtk.org/t/vtkexodusiireader-possible-bug/3346/2
 # Rendering-StringToImageDemo: Bar on right side in Windows
 list(APPEND TESTS_TO_IGNORE
   "Meshes-InterpolateFieldDataDemo"
