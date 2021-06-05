eb48a34aaa7363d92cbdb70078dc96ec7af52ed6
Andrew Maclean <andrew.amaclean@gmail.com>
2021-06-06 08:37:32 +1000

    Rename a using

--- src/Cxx/Utilities/ForLoop.cxx
+++ src/Cxx/Utilities/ForLoop.cxx
@@ -149,11 +149,11 @@ struct Mag3Worker3
     const auto vecRange = vtk::DataArrayTupleRange<3>(vecs);
     auto magRange = vtk::DataArrayValueRange<1>(mags);
 
-    using ConstVecTuple = typename decltype(vecRange)::ConstTupleReferenceType;
+    using VecConstTupleRef = typename decltype(vecRange)::ConstTupleReferenceType;
     using MagType = typename decltype(magRange)::ValueType;
 
     // Per-tuple magnitude functor for std::transform:
-    auto computeMag = [](ConstVecTuple&& tuple) -> MagType {
+    auto computeMag = [](VecConstTupleRef&& tuple) -> MagType {
       MagType mag = 0;
       for (const auto& comp : tuple)
       {
