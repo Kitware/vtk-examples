599177a58f79a991e0f9da036c6912c2720d3dfc
Andrew Maclean <andrew.amaclean@gmail.com>
2022-05-25 15:15:06 +1000

    Removing the leading "r"
    The text: r¨¨¨ causes markdown wrapping failures.

--- src/Trame/Applications/FiniteElementAnalysis/app_remote_view.py
+++ src/Trame/Applications/FiniteElementAnalysis/app_remote_view.py
@@ -1,4 +1,4 @@
-r"""
+"""
 Version for trame 1.x - https://github.com/Kitware/trame/blob/release-v1/examples/VTK/Applications/FiniteElementAnalysis/app_remote_view.py
 Delta v1..v2          - https://github.com/Kitware/trame/commit/03f28bb0084490acabf218264b96a1dbb3a17f19
 """
