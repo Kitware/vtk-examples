58a888bce6d5b1a86844cbb670dee3666c515108
Peter Franz <Peter.Franz@FlowStateSolutions.co.nz>
2022-07-20 07:28:19 +1200

    Modify CompassWidget.cxx to be more interactive.

--- src/Cxx/Widgets/CompassWidget.cxx
+++ src/Cxx/Widgets/CompassWidget.cxx
@@ -1,9 +1,9 @@
 #include <vtkActor.h>
-#include <vtkArrowSource.h>
+#include <vtkCamera.h>
+#include <vtkCommand.h>
 #include <vtkCompassRepresentation.h>
 #include <vtkCompassWidget.h>
-#include <vtkInteractorStyleTrackball.h>
-#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkCubeSource.h>
 #include <vtkNamedColors.h>
 #include <vtkNew.h>
 #include <vtkPolyData.h>
@@ -13,14 +13,50 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
 
+class vtkICWValueChangedCallback : public vtkCommand
+{
+public:
+  static vtkICWValueChangedCallback* New()
+  {
+    return new vtkICWValueChangedCallback();
+  }
+  virtual void Execute(vtkObject* caller, unsigned long eventId, void* callData)
+  {
+    vtkCompassWidget* widget = vtkCompassWidget::SafeDownCast(caller);
+    vtkCamera* camera = widget->GetCurrentRenderer()->GetActiveCamera();
+
+    // calculate new camera position from compass widget parameters
+    double distance = widget->GetDistance();
+    double tilt = widget->GetTilt();
+    double heading = widget->GetHeading();
+
+    double pos[3];
+    pos[0] = distance * cos(vtkMath::RadiansFromDegrees(heading)) *
+        cos(vtkMath::RadiansFromDegrees(tilt));
+    pos[1] = distance * sin(vtkMath::RadiansFromDegrees(heading)) *
+        cos(vtkMath::RadiansFromDegrees(tilt));
+    pos[2] = distance * sin(vtkMath::RadiansFromDegrees(tilt));
+
+    camera->SetPosition(pos);
+    camera->SetFocalPoint(0, 0, 0);
+    camera->SetViewUp(0, 0, 1);
+    camera->SetClippingRange(0.1, distance + 1);
+
+    widget->GetCurrentRenderer()->Render();
+  }
+  vtkICWValueChangedCallback()
+  {
+  }
+};
+
 int main(int, char*[])
 {
   vtkNew<vtkNamedColors> colors;
 
-  vtkNew<vtkArrowSource> arrowSource;
+  vtkNew<vtkCubeSource> cubeSource;
 
   vtkNew<vtkPolyDataMapper> mapper;
-  mapper->SetInputConnection(arrowSource->GetOutputPort());
+  mapper->SetInputConnection(cubeSource->GetOutputPort());
 
   vtkNew<vtkActor> actor;
   actor->SetMapper(mapper);
@@ -35,12 +71,20 @@ int main(int, char*[])
   vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
   renderWindowInteractor->SetRenderWindow(renderWindow);
 
-  // Create the widget and its representation
+  // create the widget and its representation
   vtkNew<vtkCompassRepresentation> compassRepresentation;
+  compassRepresentation->SetMinimumDistance(2);
+  compassRepresentation->SetMaximumDistance(10);
 
   vtkNew<vtkCompassWidget> compassWidget;
   compassWidget->SetInteractor(renderWindowInteractor);
   compassWidget->SetRepresentation(compassRepresentation);
+  compassWidget->SetDistance(5.0);
+
+  // create the callback
+  vtkNew<vtkICWValueChangedCallback> valueChangedCallback;
+  compassWidget->AddObserver(vtkCommand::WidgetValueChangedEvent,
+                             valueChangedCallback);
 
   // add the actors to the scene
   renderer->AddActor(actor);
@@ -52,8 +96,11 @@ int main(int, char*[])
   renderWindow->Render();
   compassWidget->EnabledOn();
 
-  vtkNew<vtkInteractorStyleTrackballCamera> style;
-  renderWindowInteractor->SetInteractorStyle(style);
+  // no interactor style - camera is moved by widget callback
+  renderWindowInteractor->SetInteractorStyle(nullptr);
+
+  // set camera to initial position
+  compassWidget->InvokeEvent(vtkCommand::WidgetValueChangedEvent);
 
   // begin interaction
   renderWindowInteractor->Start();
