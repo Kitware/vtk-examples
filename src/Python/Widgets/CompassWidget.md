25d87bfa5e8563e47beba5d1ce4f661af1d54e21
Peter Franz <Peter.Franz@FlowStateSolutions.co.nz>
2022-09-03 09:39:17 +1200

    CompassWidget.py works with improved vtkCompassWidget

--- src/Python/Widgets/CompassWidget.md
+++ src/Python/Widgets/CompassWidget.md
@@ -1,3 +1,5 @@
 ### Description
 
-This example creates a compass in the top right corner of the window.
+This example creates a compass widget in the top right corner of the window. The widget can be used to modify the camera position via its distance and tilt sliders and its heading compass wheel.
+
+Note for this example to work correctly VTK with version >= 9.2.20220831 is required.
--- src/Python/Widgets/CompassWidget.py
+++ src/Python/Widgets/CompassWidget.py
@@ -1,36 +1,73 @@
 #!/usr/bin/env python3
 
+# determine if the current VTK version has the improved CompassWidget
+import sys
+sys.path.append('../Utilities')
+from CheckVTKVersion import vtk_version_ok
+VTK_IMPROVED_COMPASSWIDGET = vtk_version_ok(9, 2, 20220831)
+
+import math
 # noinspection PyUnresolvedReferences
 import vtkmodules.vtkInteractionStyle
 # noinspection PyUnresolvedReferences
 import vtkmodules.vtkRenderingOpenGL2
-from vtkmodules.vtkCommonColor import vtkNamedColors
-from vtkmodules.vtkFiltersSources import vtkArrowSource
-from vtkmodules.vtkGeovisCore import (
-    vtkCompassRepresentation,
-    vtkCompassWidget
+from vtkmodules.vtkCommonCore import (
+    vtkCommand,
+    vtkMath
 )
+from vtkmodules.vtkCommonColor import vtkNamedColors
+from vtkmodules.vtkRenderingAnnotation import vtkAnnotatedCubeActor
 from vtkmodules.vtkInteractionStyle import vtkInteractorStyleTrackballCamera
 from vtkmodules.vtkRenderingCore import (
-    vtkActor,
-    vtkPolyDataMapper,
     vtkRenderWindow,
     vtkRenderWindowInteractor,
     vtkRenderer
 )
 
+if VTK_IMPROVED_COMPASSWIDGET:
+    from vtkmodules.vtkInteractionWidgets import (
+        vtkCompassRepresentation,
+        vtkCompassWidget
+    )
+else:
+    from vtkmodules.vtkGeovisCore import (
+        vtkCompassRepresentation,
+        vtkCompassWidget
+    )
+
+
+def CompassWidgetValueChangedCallback(widget, event):
+    """
+    Callback to set the camera position according to the position parameters given by the vtkCompassWidget.
+    """
+    try:
+        camera = widget.GetCurrentRenderer().GetActiveCamera()
+    except AttributeError:
+        return
+    
+    # calculate new camera position from compass widget parameters
+    distance = widget.GetDistance()
+    tilt = widget.GetTilt()
+    heading = widget.GetHeading()
+    
+    x = distance * math.cos(vtkMath.RadiansFromDegrees(heading)) * math.cos(vtkMath.RadiansFromDegrees(tilt))
+    y = distance * math.sin(vtkMath.RadiansFromDegrees(heading)) * math.cos(vtkMath.RadiansFromDegrees(tilt))
+    z = distance * math.sin(vtkMath.RadiansFromDegrees(tilt))
+    
+    camera.SetPosition(x, y, z)
+    camera.SetFocalPoint(0, 0, 0)
+    camera.SetViewUp(0, 0, 1)
+    camera.SetClippingRange(0.1, distance + 1)
+    
+    widget.GetCurrentRenderer().Render()
+
+
 
 def main():
     colors = vtkNamedColors()
 
-    arrowSource = vtkArrowSource()
-
-    mapper = vtkPolyDataMapper()
-    mapper.SetInputConnection(arrowSource.GetOutputPort())
-
-    actor = vtkActor()
-    actor.SetMapper(mapper)
-    actor.GetProperty().SetColor(colors.GetColor3d('PeachPuff'))
+    actor = vtkAnnotatedCubeActor()
+    actor.GetCubeProperty().SetColor(colors.GetColor3d('PeachPuff'))
 
     # a renderer and render window
     renderer = vtkRenderer()
@@ -41,13 +78,23 @@ def main():
     renderWindowInteractor = vtkRenderWindowInteractor()
     renderWindowInteractor.SetRenderWindow(renderWindow)
 
-    # Create the widget and its representation
+    # create the widget and its representation
     compassRepresentation = vtkCompassRepresentation()
 
     compassWidget = vtkCompassWidget()
     compassWidget.SetInteractor(renderWindowInteractor)
     compassWidget.SetRepresentation(compassRepresentation)
+    
+    # add a callback to update the camera position on vtkCommand::WidgetValueChangedEvent
+    compassWidget.AddObserver(vtkCommand.WidgetValueChangedEvent, CompassWidgetValueChangedCallback)
 
+    if VTK_IMPROVED_COMPASSWIDGET:
+        compassRepresentation.SetMinimumDistance(2)
+        compassRepresentation.SetMaximumDistance(10)
+        compassWidget.SetDistance(5)
+        compassWidget.SetTiltSpeed(45)
+        compassWidget.SetDistanceSpeed(2)
+        
     # add the actors to the scene
     renderer.AddActor(actor)
     renderer.SetBackground(colors.GetColor3d('MidnightBlue'))
@@ -58,8 +105,11 @@ def main():
     renderWindow.Render()
     compassWidget.EnabledOn()
 
-    style = vtkInteractorStyleTrackballCamera()
-    renderWindowInteractor.SetInteractorStyle(style)
+    if VTK_IMPROVED_COMPASSWIDGET:
+        # no interactor style - camera is moved by widget callback
+        renderWindowInteractor.SetInteractorStyle(None)
+        # set camera to initial position
+        compassWidget.InvokeEvent(vtkCommand.WidgetValueChangedEvent)
 
     # begin interaction
     renderWindowInteractor.Start()
